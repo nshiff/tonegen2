3d1f6ca4b879c7a0e46c480bade8be72bc15a32e
N <nshiff@users.noreply.github.com>
2016-01-09 22:57:16 -0800

    can now reproduce multiple waveforms

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 #define SAMPLES_PER_SECOND 8000;
 #define SAMPLE_DURATION 1.0/SAMPLES_PER_SECOND;
 
-char WAVEFORM_SQUARE[4] = {0x00, 0x00, 0xff, 0xff};
-
+char WAVEFORM_SQUARE[256];
+char WAVEFORM_SAWTOOTH[256];
 
 void playWavelength(int frequency, int numberOfSamplesInWavelength){
 	double wavelength = 1.0/frequency;
@@ -23,11 +23,25 @@ void playWavelength(int frequency, int numberOfSamplesInWavelength){
 
 int main(){
 
+	int i=0;
+	for(i=0; i<256; i++){
+		WAVEFORM_SAWTOOTH[i] = (char)i;
+		
+		if(i < 128){
+			WAVEFORM_SQUARE[i] = (char)0;
+		}
+		else{
+			WAVEFORM_SQUARE[i] = (char)255;
+		}
+	}
+
+
+
 	int freq = 220;
 
 	while(1==1){
 
-		playWavelength(freq, 4);
+		playWavelength(freq, 256);
 
 	}
 
