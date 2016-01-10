5495fb96fd02a7ff5653dc3e1bff039b0bae64db
N <nshiff@users.noreply.github.com>
2016-01-09 22:24:22 -0800

    refactoring for variable waveform

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ const int LENGTH = 2;
 
 
 int main(){
-	char waveform_square[2] = {0x00, 0xff};
+	char waveform_square[4] = {0x00, 0x00, 0xff, 0xff};
 	
 	int samplesPerSecond = 8000;
 	double sampleDuration = 1.0/samplesPerSecond;
@@ -18,14 +18,27 @@ int main(){
 
 	while(1==1){
 		double totalDuration =0.0;
-		while( totalDuration<(wavelength/2.0)){
+		while( totalDuration<((1*wavelength)/4.0)){
 			putchar(waveform_square[0]);
 			totalDuration += sampleDuration;
 		}
-		while( totalDuration<(wavelength/1.0)){
+
+		while( totalDuration<((2*wavelength)/4.0)){
 			putchar(waveform_square[1]);
 			totalDuration += sampleDuration;
 		}
+
+		while( totalDuration<((3*wavelength)/4.0)){
+			putchar(waveform_square[2]);
+			totalDuration += sampleDuration;
+		}
+
+		while( totalDuration<((4*wavelength)/4.0)){
+			putchar(waveform_square[3]);
+			totalDuration += sampleDuration;
+		}
+
+
 	}
 
 
