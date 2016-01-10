dacc0560a8ccf18d455dc73239eebaa77f1f52c6
N <nshiff@users.noreply.github.com>
2016-01-09 20:36:14 -0800

    Generating a square wave!

---
+++ README.md
@@ -0,0 +1,1 @@
+make && ./a.out | aplay
--- main.c
+++ main.c
@@ -6,15 +6,15 @@ int main(){
 	while(1==1){
 
 		int i=0;
-		for(i; i<128; i++){
-			printf("%d\n",i);
+		for(i; i<128; i+=4){
+			putchar(0);
 		}
-		for(i; i<=255; i++){
-			printf("%d\n",i);
-		}	
-		printf("\n");
+		for(i; i<=255; i+=4){
+			putchar(255);
+		}
 	}
 
 	return 0;
 
 }
+
